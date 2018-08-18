c995dec8fa748aab914febe0865c2b4588a0ad33
David Chiu <davidwchiu@gmail.com>
2018-08-18 19:59:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
-
 class Student < InteractiveRecord
 
   self.column_names.each do |field|
